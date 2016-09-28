265d3fa26aa85d0f93ff25f139fa83ef9ba4ec91
Benjamin <blabblabbalab@gmail.com>
2016-09-28 16:13:31 -0500

    Round two

--- app/src/main/java/com/example/ag5300cm/surveyapp/SurveyAppActivation.java
+++ app/src/main/java/com/example/ag5300cm/surveyapp/SurveyAppActivation.java
@@ -3,10 +3,13 @@ package com.example.ag5300cm.surveyapp;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.style.TtsSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class SurveyAppActivation extends AppCompatActivity {
@@ -35,8 +38,9 @@ public class SurveyAppActivation extends AppCompatActivity {
         twotally = 0;
 
 
-
-        final TextView show_entered_question = (TextView) findViewById(R.id.new_question_editText);
+        //final TextView show_entered_question = (TextView) findViewById(R.id.new_question_editText);
+        //String show_entered_question.findViewById(R.id.new_question_editText);
+        //final TextView show_entered_question;
         //final TextView show_entered_question;
 
         newSetQuestionDone_Button.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +49,11 @@ public class SurveyAppActivation extends AppCompatActivity {
                 //TODO send info to survey display
                 setContentView(R.layout.display_survey_question);
 
+                TextView entered_question = (TextView) findViewById(R.id.new_question_editText);
+                mshow_entered_question.setText(entered_question.getText());
+
+                //mshow_entered_question.setText(findViewById(R.id.new_question_editText));
+                //show_entered_question = (TextView)
                 //final TextView show_entered_question = (TextView) findViewById(R.id.new_question_editText);
 
                 //String newQuestion = newEnteredSurveyQuestion.getText().toString();
